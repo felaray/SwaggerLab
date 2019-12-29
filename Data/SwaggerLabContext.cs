96cdcfa94e6be04877b49c0b45384caebef13560
Felaray Ho <felaray@gmail.com>
2019-12-29 16:30:01 +0800

    add  ef core

---
+++ Data/SwaggerLabContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SwaggerLab.Model;
+
+namespace SwaggerLab.Data
+{
+    public class SwaggerLabContext : DbContext
+    {
+        public SwaggerLabContext (DbContextOptions<SwaggerLabContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SwaggerLab.Model.User> User { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,4 @@
-using System;
+Ôªøusing System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using SwaggerLab.Data;
 
 namespace SwaggerLab
 {
@@ -34,8 +36,11 @@ namespace SwaggerLab
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
             });
 
-            //§‰¥©Newtonsoft™∫JsonIgnore
+            //ÊîØÊè¥NewtonsoftÁöÑJsonIgnore
             services.AddSwaggerGenNewtonsoftSupport(); // explicit opt-in
+
+            services.AddDbContext<SwaggerLabContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SwaggerLabContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
